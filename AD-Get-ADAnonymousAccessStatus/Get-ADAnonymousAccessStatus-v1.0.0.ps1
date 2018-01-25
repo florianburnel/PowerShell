374a428fd98daeec1b7916ef8e11120a425f6271
florianburnel <florian.burnel@it-connect.fr>
2018-01-25 21:45:12 +0100

    Get-ADAnonymousAccessStatus-v1.0.0

---
+++ AD-Get-ADAnonymousAccessStatus/Get-ADAnonymousAccessStatus-v1.0.0.ps1
@@ -0,0 +1,39 @@
+﻿<#
+        .SYNOPSIS
+        
+         This script query the Active Directory to check the unicode value "dsHeuristics".
+         The 7th value of this settings determine if anonymous access is authorized or not in your environment.
+         So, if the value if "not defined", it's OK because equal 0. But if the value is 2, it's bad !
+        
+        .NOTES
+	     NAME:	Get-ADAnonymousAccessStatus.ps1
+	     AUTHOR:	Florian Burnel
+	     EMAIL:	florian.burnel@it-connect.fr
+         URL : www.it-connect.fr
+	     TWITTER: @FlorianBurnel
+        
+        .EXAMPLE        
+         PS> .\Get-ADAnonymousAccessStatus.ps1
+   
+        .PARAMETER
+         NO PARAMETER.
+
+
+	    VERSION HISTORY:
+	        
+        1.0.0 	2018.01.25
+	            Initial Version
+#>
+
+$TargetDN = ("CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration," + (Get-ADDomain).DistinguishedName)
+$ValuedsHeuristics = (Get-ADObject -Identity $TargetDN -Properties dsHeuristics).dsHeuristics
+
+if(($ValuedsHeuristics -eq "") -or ($ValuedsHeuristics.Length -lt 7)){
+    
+    Write-Output "Good ! Anonymous access is already disable !"
+
+}elseif(($ValuedsHeuristics.Length -ge 7) -and ($ValuedsHeuristics[6] -eq "2")){
+
+    Write-Output "Warning ! Anonymous access is enable and authorized on your Active Directory ! Value = $ValuedsHeuristics"
+
+}
---
+++ AD-Get-ADAnonymousAccessStatus/README.md
@@ -0,0 +1,16 @@
+# Get-ADAnonymousAccessStatus
+
+This script query the Active Directory to check the unicode value "dsHeuristics".
+The 7th value of this settings determine if anonymous access is authorized or not in your environment.
+So, if the value if "not defined", it's OK because equal 0. But if the value is 2, it's bad !
+		 
+# Example
+
+```
+        PS> .\Get-ADAnonymousAccessStatus.ps1
+```
+
+# Changelog
+	        
+1.0.0 	2018.01.25
+        Initial Version
