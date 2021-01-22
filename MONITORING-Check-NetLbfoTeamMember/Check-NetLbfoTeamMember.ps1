bdb4009037627a1b90c772ec66abfc5dbc680262
FlorianBurnel <florian.burnel@it-connect.fr>
2021-01-22 10:56:16 +0100

    Push MONITORING-Check-NetLbfoTeamMember

---
+++ MONITORING-Check-NetLbfoTeamMember/Check-NetLbfoTeamMember.ps1
@@ -0,0 +1,49 @@
+﻿<#
+.SYNOPSIS
+	Check status of teaming members. All teams of a local server.
+
+.DESCRIPTION
+
+.PARAMETER 
+
+.EXAMPLE
+
+.INPUTS
+
+.OUTPUTS
+	
+.NOTES
+	NAME:	Check-NetLbfoTeamMember.ps1
+	AUTHOR:	Florian Burnel
+	EMAIL:	florian.burnel@it-connect.fr
+	WWW:	www.it-connect.fr
+	Twitter:@FlorianBurnel
+
+	VERSION HISTORY:
+
+	1.0 	2021.01.22
+		    Initial Version
+
+#>
+# Initial state
+$ExitCode = 0
+
+# Get the status
+$LbfoTeamMemberOutput = Get-NetLbfoTeamMember | Select-Object Name,FailureReason,Team
+$LbfoTeamMemberCount = (Get-NetLbfoTeamMember).Count
+
+# Normal state : AdministrativeDecision / NoFailure
+Foreach($Member in $LbfoTeamMemberOutput){
+
+    if(!(($Member.FailureReason -eq "NoFailure") -or ($Member.FailureReason -eq "AdministrativeDecision"))){
+
+        $ExitCode = 2
+        Write-Output "CRITICAL: Member $($Member.Name) of the team $($Member.Team) state is $($Member.FailureReason)"
+
+    }
+}
+
+# Evaluate final exit code result for all passed checks.
+if ($ExitCode -eq 0) { Write-Output "OK: Members ($LbfoTeamMemberCount) of all LBFO teams are OK" }
+
+exit $ExitCode
---
+++ MONITORING-Check-NetLbfoTeamMember/README.md
@@ -0,0 +1,7 @@
+# Check-NetLbfoTeamMember
+
+Script à intégrer dans une solution de supervision. Sur Windows, il s'exécute par l'intermédiaire du module NRPE de NSClient++. Le script va vérifier l'état de toutes les équipes (teaming) de cartes réseaux d'un serveur local. S'il y a une carte en erreur dans une équipe, elle va remonter en erreur dans la supervision. Il permet d'identifier une carte défaillante dans une équipe. Suivre le lien ci-dessous pour plus d'informations.
+
+# Links
+
+[Supervision : comment vérifier l'état des Teaming dans Windows Server ?](https://www.it-connect.fr/supervision-comment-verifier-letat-des-teaming-dans-windows-server/)
