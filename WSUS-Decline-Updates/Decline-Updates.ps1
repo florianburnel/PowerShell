30e016abfc8a560f4816b11f338be6bebdecae5f
FlorianBurnel <florian.burnel@it-connect.fr>
2022-08-03 20:42:42 +0200

    Scripts WSUS

---
+++ WSUS-Decline-Updates/Decline-Updates.ps1
@@ -0,0 +1,106 @@
+﻿<#
+.Synopsis 
+   Sample script to decline superseded updates from WSUS, and run WSUS cleanup if any changes are made  
+
+.DESCRIPTION 
+   Declines updates from WSUS if update meets any of the following:
+        - is superseded
+        - is expired (as defined by Microsoft)
+        - is for x86 or itanium operating systems
+        - is for Windows XP
+        - is a language pack
+        - is for old versions of Internet Explorer (versions 7,8,9)
+        - contains some country names for country specific updates not filtered by WSUS language filters.
+        - is a beta update
+        - is for an embedded operating system
+
+    If an update is released for multiple operating systems, and one or more of the above criteria are met, the versions of the update that do not meet the above will not be declined by this script
+
+.EXAMPLE 
+   .\Decline-Updates -WSUSServer WSUSServer.Company.com -WSUSPort 8530
+
+# Last updated 13 July 2016
+ 
+# Author 
+Nick Eales, Microsoft
+#>
+
+
+Param(    
+    [Parameter(Mandatory=$false, 
+    ValueFromPipeline=$true, 
+    ValueFromPipelineByPropertyName=$true, 
+    ValueFromRemainingArguments=$false, 
+    Position=0)] 
+    [string]$WSUSServer = "Localhost", #default to localhost
+    [int]$WSUSPort=8530,
+    [switch]$reportonly
+    )
+
+Function Decline-Updates{
+    Param(
+        [string]$WsusServer,
+        [int]$WSUSPort,
+        [switch]$ReportOnly
+    )
+    write-host "Connecting to WSUS Server $WSUSServer and getting list of updates"
+    $Wsus = Get-WSUSserver -Name $WSUSServer -PortNumber $WSUSPort
+    if($WSUS -eq $Null){
+        write-error "unable to contact WSUSServer $WSUSServer"
+    }else{
+        $Updates = $wsus.GetUpdates()
+        write-host "$(($Updates | where {$_.IsDeclined -eq $false} | measure).Count) Updates before cleanup"
+        $updatesToDecline = $updates | where {$_.IsDeclined -eq $false -and (
+        $_.IsSuperseded -eq $true -or   #remove superseded updates
+        $_.PublicationState -eq "Expired" -or #remove updates that have been pulled by Microsoft
+        $_.LegacyName -match "ia64" -or #remove updates for itanium computers (1/2)
+        $_.LegacyName -match "x86" -or  #remove updates for 32-bit computers
+        $_.LegacyName -match "XP" -or   #remove Windows XP updates (1/2)
+        $_.producttitles -match "XP" -or #remove Windows XP updates (1/2)
+        $_.Title -match "Itanium" -or   #remove updates for itanium computers (2/2)
+        $_.Title -match "language\s" -or  #remove langauge packs
+        $_.title -match "Internet Explorer 7" -or #remove updates for old versions of IE
+        $_.title -match "Internet Explorer 8" -or 
+        $_.title -match "Internet Explorer 9" -or 
+        $_.title -match "Japanese" -or #some non-english updates are not filtered by WSUS language filtering
+        $_.title -match "Korean" -or   
+        $_.title -match "Taiwan" -or  
+        $_.Title -match "Beta" -or     #Beta products and beta updates
+        $_.title -match "Embedded"     #Embedded version of Windows
+        )}
+        
+        write-host "$(($updatesToDecline | measure).Count) Updates to decline"
+        $changemade = $false        
+        if($reportonly){
+            write-host "ReportOnly was set to true, so not making any changes"
+        }else{
+            $changemade = $true
+            $updatesToDecline | %{$_.Decline()}
+        }
+
+        #Decline updates released more then 3 months prior to the release of an included service pack
+        # - service packs updates don't appear to contain the supersedance information.
+        Foreach($SP in $($updates | where title -match "^Windows Server \d{4} .* Service Pack \d")){
+            if(($SP.ProductTitles |measure ).count -eq 1){
+                $updatesToDecline = $updates | where {$_.IsDeclined -eq $false -and $_.ProductTitles -contains $SP.ProductTitles -and $_.CreationDate -lt $SP.CreationDate.Addmonths(-3)}
+                if($updatesToDecline -ne $null){
+                    write-host "$(($updatesToDecline | measure).Count) Updates to decline (superseded by $($SP.Title))"
+                    if(-not $reportonly){
+                        $changemade = $true
+                        $updatesToDecline | %{$_.Decline()}
+                    }
+                }
+            }
+        }
+        
+        #if changes were made, run a WSUS cleanup to recover disk space
+        if($changemade -eq $true -and $reportonly -eq $false){
+            $Updates = $wsus.GetUpdates()
+            write-host "$(($Updates | where {$_.IsDeclined -eq $false} | measure).Count) Updates remaining, running WSUS cleanup"
+            Invoke-WsusServerCleanup -updateServer $WSUS -CleanupObsoleteComputers -CleanupUnneededContentFiles -CleanupObsoleteUpdates -CompressUpdates -DeclineExpiredUpdates -DeclineSupersededUpdates
+        }
+
+    }
+}
+
+Decline-Updates -WSUSServer $WSUSServer -WSUSPort $WSUSPort -reportonly:$reportonly
---
+++ WSUS-wsus-cleanup-updates-v4/wsus-cleanup-updates-v4.ps1
@@ -0,0 +1,266 @@
+﻿<#
+
+    WSUS-CLEANUP-UPDATES
+    
+    Runs WSUS cleanup task using stored procedures in WSUS database
+    thus avoiding timeout errors that may occur when running WSUS Cleanup Wizard.
+
+    The script is intended to run as a scheduled task on WSUS server
+    but can also be used remotely. $SqlServer and $SqlDB variables 
+    must be defined before running the script on a server without WSUS.
+
+    Version 4
+
+    Version history:
+
+    4    Added database connection state check before deleting an 
+         unused update: the script will now attempt to reestablish
+         connection if broken.
+
+
+#>
+
+
+##########################
+# Configurable parameters
+
+$SqlServer = ""    # SQL server host name; leave empty to use information from local registry
+$SqlDB = "SUSDB"   # WSUS database name     
+$SkipFileCleanup = $SqlServer -ne ""
+
+$log_source = "WSUS cleanup Task"  # Event log source name
+$log_debugMode = $true  # set to false to suppress console output 
+
+
+##########################
+
+
+$ErrorActionPreference = "Stop"
+
+# basic logging facility
+
+function log_init{
+    if ( -not [System.Diagnostics.EventLog]::SourceExists($log_source) ){
+        [System.Diagnostics.EventLog]::CreateEventSource($log_source, "Application")
+    }
+}
+
+function log( [string] $msg, [int32] $eventID, [System.Diagnostics.EventLogEntryType] $level ){
+    Write-EventLog -LogName Application -Source $log_source -EntryType $level -EventId $eventID -Message $msg 
+    if ( $log_debugMode ){
+        switch ($level){
+            Warning {Write-Host $msg -ForegroundColor Yellow }
+            Error { Write-Host $msg -ForegroundColor Red }
+            default { Write-Host $msg -ForegroundColor Gray }
+        }
+    }
+}
+
+function dbg( [string] $msg ){
+    if ( $log_debugMode ){ 
+        log "DBG: $msg"  300 "Information"
+    }
+}
+
+log_init
+
+
+#########################
+
+
+function DeclineExpiredUpdates( $dbconn ){
+
+    log "Declining expired updates" 1 "Information"
+
+    $Command = New-Object System.Data.SQLClient.SQLCommand 
+    $Command.Connection = $dbconn 
+    $Command.CommandTimeout = 3600
+    $Command.CommandText = "EXEC spDeclineExpiredUpdates"
+    try{
+        $Command.ExecuteNonQuery() | Out-Null
+    }
+    catch{
+        $script:errorCount++
+        log "Exception declining expired updates:`n$_" 99 "Error"
+    }
+}
+
+#########################
+
+function DeclineSupersededUpdates( $dbconn ){
+
+    log "Declining superseded updates" 1 "Information"
+    
+    $Command = New-Object System.Data.SQLClient.SQLCommand 
+    $Command.Connection = $dbconn 
+    $Command.CommandTimeout = 1800
+    $Command.CommandText = "EXEC spDeclineSupersededUpdates"
+    try{
+        $Command.ExecuteNonQuery() | Out-Null
+    }
+    catch{
+        $script:errorCount++
+        log "Exception declining superseded updates:`n$_" 99 "Error"
+    }
+}
+
+
+#######################
+
+function DeleteObsoleteUpdates( $dbconn ){
+
+        Log "Reading obsolete update list." 1 "Information"
+        $Command = New-Object System.Data.SQLClient.SQLCommand 
+        $Command.Connection = $dbconn 
+        $Command.CommandTimeout = 600
+        $Command.CommandText = "EXEC spGetObsoleteUpdatesToCleanup" 
+        $reader = $Command.ExecuteReader()
+        $table = New-Object System.Data.DataTable 
+        $table.Load($reader)
+
+        $updatesTotal = $table.Rows.Count
+        log "Found $updatesTotal updates that can be deleted." 1 "Information"
+
+        $updatesProcessed=0
+        $Command.CommandTimeout = 300
+        foreach( $row in $table.Rows ){
+            try{
+                if ( $dbconn.State -ne [System.Data.ConnectionState]::Open ){
+                    log "Re-opening database connection" 2 "Warning"
+                    $dbconn.Open()
+                }
+                $updatesProcessed++
+                log "Deleting update $($row.localUpdateID) ($updatesProcessed of $updatesTotal)" 1 "Information"
+                $Command.CommandText = "exec spDeleteUpdate @LocalUpdateID=$($row.localUpdateID)"
+                $Command.ExecuteNonQuery() | Out-Null
+            }
+            catch{
+                $errorCount++
+                log "Error deleting update $($row.localUpdateID):`n$_" 8 "Warning"
+            }
+        }
+
+}
+
+###################
+
+
+function DbConnectionString{
+
+    $WsusSetupKey = "HKLM:\SOFTWARE\Microsoft\Update Services\Server\Setup"
+
+    if ( $script:SqlServer -eq "" ){
+        $server = Get-ItemProperty -path $WsusSetupKey -Name "SqlServerName" -ErrorAction SilentlyContinue
+        $db = Get-ItemProperty -path $WsusSetupKey -Name "SqlDatabaseName" -ErrorAction SilentlyContinue
+        if ( ! $server  ){
+            throw "Cannot determine SQL server name" 
+        }
+        $script:SqlServer = $server.SqlServerName
+        $script:SqlDB = $db.SqlDatabaseName
+    }
+
+    if ( $script:SqlServer -match "microsoft##" ){
+        return "data source=\\.\pipe\$script:SqlServer\tsql\query;Integrated Security=True;database='$script:SqlDB';Network Library=dbnmpntw"
+    }
+    else{
+        return "server='$script:SqlServer';database='$script:SqlDB';trusted_connection=true;" 
+    }
+
+}
+
+
+##############
+
+function DeleteUnusedContent{
+
+    log "Deleting unneeded content files" 1 "Information"
+    
+    try{
+        Import-Module UpdateServices
+        $status = Invoke-WsusServerCleanup -CleanupUnneededContentFiles 
+        log "Done deleting unneeded content files: $status" 1 "Information"
+    }
+    catch{
+        $script:errorCount++
+        log "Exception deleting unneeded content files:`n$_" 99 "Error"
+    }
+
+}
+
+
+###################
+
+function DeleteInactiveComputers( $DbConn ){
+
+    log "Removing obsolete computers" 1 "Information"
+    
+    $Command = New-Object System.Data.SQLClient.SQLCommand 
+    $Command.Connection = $dbconn 
+    $Command.CommandTimeout = 1800
+    $Command.CommandText = "EXEC spCleanupObsoleteComputers"
+    try{
+        $Command.ExecuteNonQuery() | Out-Null
+    }
+    catch{
+        $script:errorCount++
+        log "Exception removing obsolete computers:`n$_" 99 "Error"
+    }
+
+}
+
+function RestartWsusService{
+    log "Stopping IIS.." 1 "Information"
+    try{
+        Stop-Service W3SVC -Force
+        try{
+            log "Restarting WSUS service.." 1 "Information"
+            Restart-Service WsusService -Force 
+        }
+        finally{
+            log "Starting IIS..." 1 "Information"
+            Start-Service W3SVC 
+        }
+    }
+    catch{
+        $script:errorCount++
+        log "Error restarting WSUS services:`n$_" 99 "Error"        
+    }
+    Start-Sleep -Seconds 30
+}
+
+<#------------------------------------------------
+                     MAIN                         
+-------------------------------------------------#>
+
+
+$timeExecStart = Get-Date
+$errorCount = 0
+
+try{
+    
+    $Conn = New-Object System.Data.SQLClient.SQLConnection 
+    $Conn.ConnectionString = DbConnectionString
+    log "Connecting to database $SqlDB on $SqlServer" 1 "Information"
+    $Conn.Open() 
+    try{
+        DeclineExpiredUpdates $Conn
+        DeclineSupersededUpdates $Conn
+        DeleteObsoleteUpdates $Conn
+        DeleteInactiveComputers $Conn   
+        RestartWsusService   
+        if ( ! $SkipFileCleanup ) {  
+            DeleteUnusedContent 
+        }
+    }
+    finally{
+        $Conn.Close() 
+    }
+
+}
+catch{
+    $errorCount++
+    log "Unhandled exception:`n$_" 100 "Error"
+}
+
+$time_exec = ( Get-Date ) - $timeExecStart
+log "Completed script execution with $errorCount error(s)`nExecution time $([math]::Round($time_exec.TotalHours)) hours and $([math]::Round($time_exec.totalMinutes)) minutes." 1 "Information"
